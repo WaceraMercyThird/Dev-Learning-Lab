f95540318d6a11f87ee040dac047a749f2bb271e
Mercy Wacera <waceraaamercy@gmail.com>
2025-09-05 11:58:24 -0700

    Functionalities

---
+++ JavaScript/Functionalities/script.js
@@ -0,0 +1,8 @@
+function calculate() {
+
+    let a = Number(document.getElementById('num1').value);
+    let b = Number(document.getElementById('num2').value);
+
+    document.getElementById('result').innerText = "result: " + (a +b );
+
+};

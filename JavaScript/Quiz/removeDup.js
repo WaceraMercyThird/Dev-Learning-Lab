85e34b7dac97f34c2ee7208d31045e9fdf388f8a
Mercy Wacera <waceraaamercy@gmail.com>
2025-09-11 20:19:13 -0700

    Add solved problems

--- JavaScript/Quiz/removeDup.js
+++ JavaScript/Quiz/removeDup.js
@@ -5,10 +5,20 @@ var removeDuplicate = (nums) => {
 };
 
 var reDuplicates = (nums) => {
-    const unique = nums.filter((n, i) => i === 0 || n !== nums[i - 1]);
+    const unique = nums.sort().filter((n, i) => i === 0 || n !== nums[i - 1]);
     nums.splice(0, unique.length, ...unique);
     return unique;
 };
+
+var removeDuplicates = (nums) => {
+    // nums.sort();
+    const unique = [...new Set(nums)];
+    nums.splice(0, unique.length, ...unique);
+    return unique
+    
+
+};
+
 var removeDup = (n) => {
     return [...new Set(n)];
 };
@@ -17,3 +27,5 @@ var removeDup = (n) => {
 console.log(removeDuplicate([0,1,1,1,9,2,1])); 
 console.log(removeDup([0,1,1,1,9,2,1])); 
 console.log(reDuplicates([0,1,1,1,9,2,1])); 
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4, 0,1]));
+
